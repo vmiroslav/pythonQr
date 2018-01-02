9593ebe55612be63bb5af503889806049952bfe3
Miroslav Vasilev <miroslav@Miroslavs-MacBook-Air.local>
2018-01-03 01:17:02 +0200

    create Class for qrCode data initialisation and image/png generation

---
+++ baseData.py
@@ -0,0 +1,6 @@
+import qrcodeTest
+"""
+	defining test object and play with it
+"""
+testObj = qrcodeTest.qrGenerator("Miroslav" ,"Vasilev", "Python Dev", "miroslav.vasilev@gmail.com", "0888888888","http://www.miroslav.com","github.com/vmiroslav")
+
--- qrcodeTest.py
+++ qrcodeTest.py
@@ -1,25 +1,43 @@
 # qrcodeTest.py
 
 import qrcode
+from PIL import Image
 
-qr = qrcode.QRCode(
-    version=1,
-    error_correction=qrcode.constants.ERROR_CORRECT_L,
-    box_size=10,
-    border=4,
-)
+class qrGenerator():
+	def  __init__(self, first_name, last_name, job_title,email,phone, website_url, git_url):
+		self.first_name 	= first_name 
+		self.last_name		= last_name
+		self.full_name		= first_name+" "+last_name
+		self.job_title 		= job_title
+		self.website_url	= website_url
+		self.git_url		= git_url
+		self.email			= email
+		self.phone			= phone
+		qrobj				=	qrcode.QRCode(
+										   version=1,
+										   error_correction=qrcode.constants.ERROR_CORRECT_L,
+										   box_size=10,
+										   border=4,
+										)
+		self.qrs 			= qrobj
 
-data = ["Miroslav Vasilev", "Python Developer" ,  "http://www.qello.com", "https://github.com/vmiroslav"]
+	def createQr(self):
+		print("Start generating QR Code .")
 
-for item in data:
-	qr.add_data(item+"\n")
+		qr_data = [self.full_name+"\n", self.job_title+"\n", self.email+"\n", self.phone+"\n", self.website_url+"\n", self.git_url+"\n"]
+		for items in qr_data:
+			self.qrs.add_data(items)
 
+		self.qrs.make(fit=True)
 
-qr.make(fit=True)
+	def generateImage(self):
+		print("Start Generating Image .")
+		image = self.qrs.make_image()
+		print("Saving %s" % self.first_name+".png")
+		image.save(self.first_name+".png")
+		image.close()
+		print("Image %s" % self.first_name+".png" + " was created .")
 
-img  = qr.make_image()
-file_name = "miro-qr.png"
-print('Saving %s' % file_name)
-image_file = open(file_name, "w")
-img.save(file_name)
-image_file.close()
+	def showQr(self):
+		qrImage = Image.open(self.first_name+'.png')
+		qrImage.show()
